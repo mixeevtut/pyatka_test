d78659b478ba0be619759e3501ebb02c9864b9e4
Кирилл Михеев <mixeev_kirill@outlook.com>
2020-03-17 16:30:09 +0300

    refactoring

--- signin_ui.py
+++ signin_ui.py
@@ -3,46 +3,46 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-class Test_1():
-    phone = 9057199558
-    card = 7789004125415905
+class SignIn():
+    phone = 9251539919
+    card = 7789004922000462
     url = 'https://5ka:eish7ohzo5Esh@mystage.5ka.ru/login'
-    password = '5ka5ka'
+    password = 'Qqwerty123'
     code = '0000'
 
     def setup_method(self):
         self.driver = webdriver.Chrome()
         self.vars = {}
 
-    def teardown_method(self, method):
+    def teardown_method(self):
         self.driver.quit()
 
-    def test_1(self):
+    def signin(self):
         self.first_factor_auth_card()
         time.sleep(2)
         self.second_factor_auth()
         time.sleep(3)
 
     def first_factor_auth_phone(self):
-        self.driver.get(Test_1.url)
+        self.driver.get(SignIn.url)
         time.sleep(2)
         self.driver.set_window_size(1920, 1057)
-        self.driver.find_element(By.ID, "authPhone").send_keys(Test_1.phone)
-        self.driver.find_element(By.NAME, "phone_password").send_keys(Test_1.password)
+        self.driver.find_element(By.ID, "authPhone").send_keys(SignIn.phone)
+        self.driver.find_element(By.NAME, "phone_password").send_keys(SignIn.password)
         self.driver.find_element(By.CSS_SELECTOR, "div:nth-child(3) .form__submit-button").click()
 
     def first_factor_auth_card(self):
-        self.driver.get(Test_1.url)
+        self.driver.get(SignIn.url)
         time.sleep(1)
         self.driver.set_window_size(1920, 1057)
         self.driver.find_element(By.CSS_SELECTOR, ".tabs__item:nth-child(2) > .tabs__item-name").click()
-        self.driver.find_element(By.ID, "authCard").send_keys(Test_1.card)
-        self.driver.find_element(By.NAME, "cardNo_password").send_keys(Test_1.password)
+        self.driver.find_element(By.ID, "authCard").send_keys(SignIn.card)
+        self.driver.find_element(By.NAME, "cardNo_password").send_keys(SignIn.password)
         time.sleep(1)
         self.driver.find_element(By.CSS_SELECTOR, "div:nth-child(4) .form__submit-button").click()
 
     def second_factor_auth(self):
         self.driver.find_element(By.NAME, "code").click()
-        self.driver.find_element(By.NAME, "code").send_keys(Test_1.code)
+        self.driver.find_element(By.NAME, "code").send_keys(SignIn.code)
         time.sleep(1)
         self.driver.find_element(By.CSS_SELECTOR, ".form__submit-button").click()
